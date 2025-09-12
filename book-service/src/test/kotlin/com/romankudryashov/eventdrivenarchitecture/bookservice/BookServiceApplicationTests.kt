e33760845ca584e5a81ecf5870b9741ee70dcf41
KhanhVo <khanh.vo.ext@sephora.sg>
2025-09-12 23:12:41 +0700

    remove test

--- book-service/src/test/kotlin/com/romankudryashov/eventdrivenarchitecture/bookservice/BookServiceApplicationTests.kt
+++ book-service/src/test/kotlin/com/romankudryashov/eventdrivenarchitecture/bookservice/BookServiceApplicationTests.kt
@@ -1,13 +1,13 @@
-package com.romankudryashov.eventdrivenarchitecture.bookservice
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class BookServiceApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+//package com.romankudryashov.eventdrivenarchitecture.bookservice
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class BookServiceApplicationTests {
+//
+//    @Test
+//    fun contextLoads() {
+//    }
+//
+//}
--- user-service/src/test/kotlin/com/romankudryashov/eventdrivenarchitecture/userservice/UserServiceApplicationTests.kt
+++ user-service/src/test/kotlin/com/romankudryashov/eventdrivenarchitecture/userservice/UserServiceApplicationTests.kt
@@ -1,13 +1,13 @@
-package com.romankudryashov.eventdrivenarchitecture.userservice
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class UserServiceApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+//package com.romankudryashov.eventdrivenarchitecture.userservice
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class UserServiceApplicationTests {
+//
+//    @Test
+//    fun contextLoads() {
+//    }
+//
+//}
