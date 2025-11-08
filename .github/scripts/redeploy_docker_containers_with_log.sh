b98fbff5452d9d0e964601ce63173399d8aca728
KhanhVo <khanh.vo.ext@sephora.sg>
2025-11-08 21:47:40 +0700

    remove files

--- .github/scripts/redeploy_docker_containers_with_log.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "------------------------------" >> /tmp/redeployment.log 2>&1
-echo "Redeployment with logs started at $(date)" >> /tmp/redeployment.log 2>&1
-~/event-driven-architecture/.github/scripts/redeploy_docker_containers.sh >> /tmp/redeployment.log 2>&1
